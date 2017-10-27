7928528d47ded14bd2c7088dce151595ed8450cc
neo <yhh1125@gmail.com>
2017-10-27 11:31:00 +0900

    sample update

--- sample/src/main/java/com/neo1125/numberkeyviewsample/MainActivity.java
+++ sample/src/main/java/com/neo1125/numberkeyviewsample/MainActivity.java
@@ -3,22 +3,38 @@ package com.neo1125.numberkeyviewsample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.neo1125.numberkeyview.NumberKeyOnClickListener;
 import com.neo1125.numberkeyview.NumberKeyView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
 
         NumberKeyView numberKeyView = (NumberKeyView) findViewById(R.id.numberKey);
         numberKeyView.setOnNumberKeyOnClickListener(new NumberKeyOnClickListener() {
             @Override
             public void onClick(NumberKeyView.Key key) {
-                Log.d("TEST", "############### key : " + key);
+                String text = textView.getText().toString();
+                switch (key) {
+                    case clear:
+                        if (text != null && !text.equals(""))
+                            textView.setText(text.substring(0, text.length()-1));
+                        break;
+                    case custom:
+                        break;
+                    default:
+                        textView.setText(text + key.toString());
+                        break;
+                }
+
             }
         });
     }
